41f0fe075fe689726152889311a6b820c3a78721
erastus <marr0kin@hotmail.com>
2017-07-20 23:20:56 -0500

    Update load_stats.php

--- application/hooks/load_stats.php
+++ application/hooks/load_stats.php
@@ -11,7 +11,7 @@ function load_stats()
         $d = preg_replace('/\$Id:\s.*?\s\$/', '$Id$', $footer_tags);
         $session_sha1 = sha1("blob " .strlen( $d ). "\0" . $d);
 
-        $CI->session->set_userdata('session_sha1', substr($session_sha1, 0, 7));
+        $CI->session->set_userdata('session_sha1', strtoupper(substr($session_sha1, 0, 7)));
         echo localStorage_setItem('session_sha1', substr($session_sha1, 0, 7));
         echo localStorage_setItem('csrf_token_name', $CI->security->get_csrf_token_name());
         echo localStorage_setItem('csrf_cookie_name', $CI->config->item('csrf_cookie_name'));
