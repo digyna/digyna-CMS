93bcd31fdd163899239bb508aa6ac1966ef4692d
erastus <marr0kin@hotmail.com>
2017-07-20 22:37:02 -0500

    Update footer.php

--- application/views/mypanel/includes/footer.php
+++ application/views/mypanel/includes/footer.php
@@ -1,5 +1,5 @@
 	</div>
-
+	<!-- END WRAPPER -->
 	<footer id="footer">
 		<p><?php echo $this->lang->line('common_you_are_using'); ?><?php echo $this->config->item('application_version'); ?> - <?php echo substr('$Id$', 5, 7); ?></p>
 	</footer>
